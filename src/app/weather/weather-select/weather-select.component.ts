db867b2a4264d4696081c521f6ad120b9687c44c
pherrero <pherrero@vexiza.com>
2022-11-02 18:57:34 +0100

    fix: fix bug in Bootstrap 3 implementation

--- src/app/weather/weather-select/weather-select.component.ts
+++ src/app/weather/weather-select/weather-select.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-weather-select',
   templateUrl: './weather-select.component.html',
-  styleUrls: ['./weather-select.component.css']
+  styleUrls: ['./weather-select.component.css'],
 })
 export class WeatherSelectComponent implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
 }
